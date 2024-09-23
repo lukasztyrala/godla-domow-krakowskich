597088d92571a90b95e1fdfc99b36649ae5afb49
lukasz <l.tyrala@gmail.com>
2024-09-23 09:11:13 +0200

    Config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://godla-domow-krakowskich.github.io',
+  base: 'godla-domow-krakowskich',
+});
